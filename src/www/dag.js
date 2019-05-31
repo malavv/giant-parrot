663e4937a95eb354bf0441d724af8a339d6c1a64
Maxime Lavigne (malavv) <duguigne@gmail.com>
2019-05-31 13:08:04 -0400

    Added node pruning

--- src/www/dag.js
+++ src/www/dag.js
@@ -1,3 +1,12 @@
+const unique = arr => [...new Set(arr)];
+const emptyCallback = () => {};
+class Queue {
+    constructor() { this.data = []; }
+    enqueue(o) { this.data.unshift(o); }
+    dequeue() { return this.data.pop(); }
+    size() { return this.data.length; }
+}
+
 export class DAG {
     constructor(idFun) {
         this.id2idx = new Map();
@@ -7,6 +16,11 @@ export class DAG {
         this.len = new Map();
     }
 
+    /**
+     * Adds node if missing
+     * @param node
+     * @returns {boolean} True if its new.
+     */
     addNode(node) {
         const id = this.idFun(node);
         if (this.id2idx.has(id))
@@ -16,22 +30,69 @@ export class DAG {
         return true; /* is new */
     }
 
+    hasLink(src, dst) {
+        const
+            sid = this.idFun(src),
+            did = this.idFun(dst);
+
+        return this.links.some(lk => nodeID(lk.source) === sid && nodeID(lk.target) === did);
+    }
+
     addLink(srcNode, dstNode) {
         const srcID = this.idFun(srcNode);
         const dstID = this.idFun(dstNode);
-        const srcIdx = this.id2idx.get(srcID);
-        const dstIdx = this.id2idx.get(dstID);
 
-        if (this.links.some(lk => lk.source === srcIdx && lk.target === dstIdx))
+        if (this.hasLink(srcNode, dstNode))
             return;
 
-        this.links.push({source: srcIdx, target: dstIdx});
-        this.len.set([srcID, dstID].join("~"), 1);
+        // get from the known nodes instead of just entering them.
+        this.links.push({
+            source: this.nodes[this.id2idx.get(srcID)],
+            target: this.nodes[this.id2idx.get(dstID)]
+        });
 
+        // Update path length
+        this.len.set([srcID, dstID].join("~"), 1);
         for (const [key, value] of this.len.entries()) {
             const [sid, did] = key.split('~');
             if (did === srcID)
                 this.len.set([sid, dstID].join("~"), value + 1);
+        }
+    }
+
+    pruneNode(node) {
+        const id = this.idFun(node);
+        const idx = this.id2idx.get(id);
+        // Remove Node
+        this.nodes.splice(idx, 1);
+        // Adjust Idx Map
+        this.nodes.map(n => this.idFun(n))
+            .forEach((nid, nidx) => { this.id2idx.set(nid, nidx); });
+        // Remove links?
+        this.links = this.links.filter(lk => this.idFun(lk.source) !== id);
+        this.links = this.links.filter(lk => this.idFun(lk.target) !== id);
+    }
+
+    rmLinksWithSourceID(id) {
+        // fix that the links are moved to the node themselves as soon as the graph loads.
+        if (this.links[0].source.id === undefined)
+            return; // Prior to the graph loading.
+
+        // Actually removing the links
+        const toBeRemoved = this.links.filter(l => this.idFun(l.source) === id);
+        this.links = this.links.filter(l => this.idFun(l.source) !== id);
+
+        // Investigate disconnected nodes for possible separate forests.
+        const nodesInSepForest = new Set();
+        for (let lk of toBeRemoved) {
+            if (isFinite(this.shortestPath(lk.source, lk.target)))
+                continue;
+            // Here the target is part of a separate forest;
+            this.bfs(lk.target, id => { nodesInSepForest.add(id); });
+        }
+
+        for (let n of nodesInSepForest) {
+            this.pruneNode(this.nodes[this.id2idx.get(n)]);
         }
     }
 
@@ -46,6 +107,53 @@ export class DAG {
             return len;
 
         return 5;
+    }
+
+    _adj(sID) {
+        return this.links.filter(lk => this.idFun(lk.source) === sID).map(lk => this.idFun(lk.target));
+    }
+
+    shortestPath(src, dst) {
+        let shortest = Number.POSITIVE_INFINITY;
+        this.bfs(src, (id, dist) => {
+            if (id === this.idFun(dst))
+                shortest = dist;
+        });
+        return shortest;
+    }
+
+    bfs(center, callback = emptyCallback) {
+        const colors = new Map();
+        const dists = new Map();
+        const parents = new Map();
+
+        const s = this.idFun(center);
+        for (let u of this.nodes.map(n => this.idFun(n))) {
+            colors.set(u, "white");
+            dists.set(u, Number.POSITIVE_INFINITY);
+            parents.set(u, null);
+        }
+
+        colors.set(s, "gray");
+        dists.set(s, 0);
+        parents.set(s, null);
+
+        const q = new Queue();
+        q.enqueue(s);
+
+        while (q.size() > 0) {
+            let u = q.dequeue();
+            for (let v of this._adj(u)) {
+                if (colors.get(v) !== "white")
+                    continue;
+                colors.set(v, "gray");
+                dists.set(v, u.d + 1);
+                parents.set(v, u);
+                q.enqueue(v);
+            }
+            colors.set(u, "gray");
+            callback(u, dists.get(u), parents.get(u) ? parents.get(u) : null);
+        }
     }
 }
 
